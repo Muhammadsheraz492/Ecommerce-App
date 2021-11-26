30a76f6433175bfd822dde1993d8ebe58b155cc9
Muhammadsheraz492 <90733116+Muhammadsheraz492@users.noreply.github.com>
2021-11-27 01:54:09 +0500

    SPlash Screen Completed

---
+++ Screens/Splash.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+import SplashComponents from "../Components/SplashComponents";
+const Splash = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <SplashComponents Name={"Lifeo"} />
+    </View>
+  );
+};
+
+export default Splash;
